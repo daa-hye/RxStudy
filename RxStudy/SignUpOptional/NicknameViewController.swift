2cdfd0abe849d485acf26ec9891c34330db4f9a4
dahye <bbngsk@gmail.com>
2023-11-01 17:21:41 +0900

    NicknameView validation 적용

--- RxStudy/SignUpOptional/NicknameViewController.swift
+++ RxStudy/SignUpOptional/NicknameViewController.swift
@@ -7,19 +7,28 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class NicknameViewController: UIViewController {
-   
+
+    let disposeBag = DisposeBag()
+
     let nicknameTextField = SignTextField(placeholderText: "닉네임을 입력해주세요")
     let nextButton = PointButton(title: "다음")
-    
+
+    let buttonColor = BehaviorSubject(value: UIColor.lightGray)
+    let validation = BehaviorSubject(value: false)
+    let nicknameValue = BehaviorSubject(value: "")
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = Color.white
         
         configureLayout()
-       
+        bind()
+
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
 
     }
@@ -28,7 +37,33 @@ class NicknameViewController: UIViewController {
         navigationController?.pushViewController(BirthdayViewController(), animated: true)
     }
 
-    
+    func bind() {
+
+        buttonColor
+            .bind(to: nextButton.rx.backgroundColor)
+            .disposed(by: disposeBag)
+
+        validation
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+
+        nicknameTextField.rx.text.orEmpty
+            .bind(with: self) { owner, value in
+                owner.nicknameValue.onNext(value)
+            }
+            .disposed(by: disposeBag)
+
+        nicknameValue
+            .map {(2...6).contains($0.count)}
+            .bind(with: self) { owner, value in
+                owner.validation.onNext(value)
+
+                let color = value ? UIColor.black : UIColor.lightGray
+                owner.buttonColor.onNext(color)
+            }
+            .disposed(by: disposeBag)
+    }
+
     func configureLayout() {
         view.addSubview(nicknameTextField)
         view.addSubview(nextButton)
