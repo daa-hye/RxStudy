0417a06234f76f562f581c3dda107add27365617
dahye <bbngsk@gmail.com>
2023-11-01 16:14:31 +0900

    birthdayView validation 완료

--- RxStudy/SignUpOptional/BirthdayViewController.swift
+++ RxStudy/SignUpOptional/BirthdayViewController.swift
@@ -7,9 +7,13 @@
  
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class BirthdayViewController: UIViewController {
-    
+
+    let disposeBag = DisposeBag()
+
     let birthDayPicker: UIDatePicker = {
         let picker = UIDatePicker()
         picker.datePickerMode = .date
@@ -36,7 +40,6 @@ class BirthdayViewController: UIViewController {
     
     let yearLabel: UILabel = {
        let label = UILabel()
-        label.text = "2023년"
         label.textColor = Color.black
         label.snp.makeConstraints {
             $0.width.equalTo(100)
@@ -46,7 +49,6 @@ class BirthdayViewController: UIViewController {
     
     let monthLabel: UILabel = {
        let label = UILabel()
-        label.text = "33월"
         label.textColor = Color.black
         label.snp.makeConstraints {
             $0.width.equalTo(100)
@@ -56,7 +58,6 @@ class BirthdayViewController: UIViewController {
     
     let dayLabel: UILabel = {
        let label = UILabel()
-        label.text = "99일"
         label.textColor = Color.black
         label.snp.makeConstraints {
             $0.width.equalTo(100)
@@ -65,14 +66,25 @@ class BirthdayViewController: UIViewController {
     }()
   
     let nextButton = PointButton(title: "가입하기")
+
+    let pickedDate = BehaviorSubject(value: Date())
     
+    let year = BehaviorSubject(value: 0)
+    let month = BehaviorSubject(value: 0)
+    let day = BehaviorSubject(value: 0)
+
+    let buttonColor = BehaviorSubject(value: UIColor.lightGray)
+    let age = BehaviorSubject(value: 0.0)
+    let validation = BehaviorSubject(value: false)
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = Color.white
         
         configureLayout()
-        
+        bind()
+
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
     }
     
@@ -80,7 +92,67 @@ class BirthdayViewController: UIViewController {
         print("가입완료")
     }
 
-    
+    func bind() {
+
+        age
+            .map { $0 >= 17 }
+            .bind(with: self) { owner, value in
+                owner.validation.onNext(value)
+                
+                let color = value ? UIColor.black : UIColor.lightGray
+                owner.buttonColor.onNext(color)
+            }
+            .disposed(by: disposeBag)
+
+        validation
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+
+        buttonColor
+            .bind(to: nextButton.rx.backgroundColor)
+            .disposed(by: disposeBag)
+
+        birthDayPicker.rx.date
+            .bind(to: pickedDate)
+            .disposed(by: disposeBag)
+
+        year
+            .map {"\($0)년"}
+            .bind(to: yearLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        month
+            .map {"\($0)월"}
+            .bind(to: monthLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        day
+            .map {"\($0)일"}
+            .bind(to: dayLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        pickedDate
+            .bind(with: self) { owner, date in
+                let component = Calendar.current.dateComponents([.year, .month, .day], from: date)
+
+                owner.year.onNext(component.year ?? 0)
+                owner.month.onNext(component.month ?? 0)
+                owner.day.onNext(component.day ?? 0)
+            }
+            .disposed(by: disposeBag)
+
+        pickedDate
+            .bind(with: self) { owner, date in
+
+                let interval: Double = date.timeIntervalSinceNow
+                let age = abs(interval / (60 * 60 * 24 * 365))
+                owner.age.onNext(age)
+
+            }
+            .disposed(by: disposeBag)
+
+    }
+
     func configureLayout() {
         view.addSubview(infoLabel)
         view.addSubview(containerStackView)
